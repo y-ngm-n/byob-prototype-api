5c4f6b40ab5fc416603f971cccdd332e314bd0bb
y-ngm-n <wogitopp@naver.com>
2024-04-17 14:06:47 +0900

    logger 추가

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import crawler from "./crawl";
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
